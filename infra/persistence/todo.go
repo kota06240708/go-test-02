e3beb5370c91fda688d6e92f2cee651b525c3e2f
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-12-13 01:40:50 +0900

    add: ユーザーのユースケースを作成

--- infra/persistence/todo.go
+++ infra/persistence/todo.go
@@ -15,9 +15,9 @@ func NewTodoPersistence() repository.TodoRepository {
 	return &TodoPersistence{}
 }
 
-//値レシーバを使ってtpがGetAllを実装
+// 値レシーバを使ってtpがGetAllを実装
 func (tp TodoPersistence) GetAll(*gin.Context) ([]*model.Todo, error) {
-	//今回はDB接続を使わずに簡単に済ませた
+	// 今回はDB接続を使わずに簡単に済ませた
 	todo1 := model.Todo{Id: 1, Title: "a", Author: "Bob", CreatedAt: time.Now().Add(-24 * time.Hour)}
 	todo2 := model.Todo{Id: 2, Title: "b", Author: "Alisa", CreatedAt: time.Now().Add(-24 * time.Hour)}
 
---
+++ usecase/user.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"github.com/jinzhu/gorm"
+
+	"github.com/api/domain/model"
+	"github.com/api/domain/repository"
+)
+
+type UserUseCase interface {
+	GetUserAll(*gorm.DB) ([]*model.User, error)
+	AddUser(DB *gorm.DB, name string, age int, icon string) error
+}
+
+type userUseCase struct {
+	userRepository repository.UserRepository
+}
+
+// ここでドメイン層のインターフェースとユースケース層のインターフェースをつなげている。
+func NewUserseCase(uu repository.UserRepository) UserUseCase {
+	return &userUseCase{
+		userRepository: uu,
+	}
+}
+
+// userデータを全件取得するためのユースケース
+func (uu userUseCase) GetUserAll(DB *gorm.DB) (users []*model.User, err error) {
+	// DBからデータを全て取得
+	users, err = uu.userRepository.GetAll(DB)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+// userデータを追加するユースケース
+func (uu userUseCase) AddUser(DB *gorm.DB, name string, age int, icon string) (err error) {
+	// DBにデータを追加
+	err = uu.userRepository.AddUser(DB, name, age, icon)
+
+	return err
+}
